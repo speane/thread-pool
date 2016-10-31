f6c5144e101c587bf784134b3f9d8be999a4501b
speane <avtolubitel225@gmail.com>
2016-10-31 03:17:18 +0300

    Created ThreadPool class

--- ThreadPool.cpp
+++ ThreadPool.cpp
@@ -1,11 +1,61 @@
-// ThreadPool.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
+#include "windows.h"
+#include "vector"
+#include "string"
+#include "iostream"
 
+class ThreadPool {
+public:
+	ThreadPool(int count) {
+		if (count < 0) {
+			logWarn("Thread pool size < 0");
+			threadCount = DEFAULT_THREAD_COUNT;
+		}
+		else {
+			threadCount = count;
+		}
+
+		HANDLE tempThread;
+		for (int i = 0; i < threadCount; i++) {
+			tempThread = CreateThread(NULL, 0, threadWork, static_cast<LPVOID>(this), 0, NULL);
+			threads.push_back(tempThread);
+		}
+
+		logInfo(threadCount + " threads started");
+	}
+
+private:
+	const int DEFAULT_THREAD_COUNT = 1;
+
+	int threadCount;
+	std::vector<HANDLE> threads;
+
+	static DWORD WINAPI threadWork(LPVOID lpParam) {
+
+	}
+	
+	static void logInfo(std::string message) {
+		std::cout << "[INFO]: " << message;
+	}
+
+	static void logWarn(std::string message) {
+		std::cout << "[WARN]: " << message;
+	}
+
+	static void logError(std::string message) {
+		std::cout << "[ERROR]: " << message;
+	}
+};
 
 int main()
 {
-    return 0;
+	int threadPoolSize;
+
+	std::cout << "Please, enter thread pool size: ";
+	std::cin >> threadPoolSize;
+
+	ThreadPool threadPool = ThreadPool(threadPoolSize);
+
+	std::cout << "Please choose task to execute(1 - task1, 2 - task2, 3 - task3)";
 }
 
